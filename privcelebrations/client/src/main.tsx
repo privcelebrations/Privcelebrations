3cab4091555630a27207029b99be265cf516a89a
privcelebrations <privcelebrations@gmail.com>
2025-08-07 00:54:12 +0530

    Update main.tsx

--- privcelebrations/client/src/main.tsx
+++ privcelebrations/client/src/main.tsx
@@ -3,3 +3,5 @@ import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
+// Prevent Replit banner from loading
+window.REPLIT_DEV_BANNER_DISABLED = true;
